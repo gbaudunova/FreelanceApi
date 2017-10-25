200e1a702edaacb8cef2ff8ba08ca3cb9a5fa5c4
Gulbustan <gbaudunova@gmail.com>
2017-10-25 17:24:14 +0600

    Updated serializers

--- api/serializers.py
+++ api/serializers.py
@@ -6,13 +6,15 @@ from rest_framework import serializers
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('first_name', 'balance')
+        fields = ('first_name', 'balance', 'id')
 
+        # customer = Customer(code='foo = "bar"\n')
+        # customer.save()
 
 class ExecuterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Executer
-        fields = ('first_name', 'balance')
+        fields = ('first_name', 'balance', 'id')
 
 
 class TaskSerializer(serializers.ModelSerializer):
