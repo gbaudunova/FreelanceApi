99e9a4f87d302ddf6f4e91e856102b16c07b1fb0
Gulbustan <gbaudunova@gmail.com>
2017-10-26 13:40:39 +0600

    modified models

--- users/models.py
+++ users/models.py
@@ -3,12 +3,15 @@ from django.db.models import F
 
 
 
+
 class Executer(models.Model):
     first_name = models.CharField(max_length=100, verbose_name='name')
     balance = models.DecimalField(max_digits=6, decimal_places=1, verbose_name='balance')
 
     def __str__(self):
         return self.first_name
+
+
     def update_balance(self, balance):
         Executer.objects.select_for_update(). \
             filter(pk=self.pk) \
@@ -21,6 +24,7 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.first_name
+
 
     def update_balance(self, balance):
         Customer.objects.select_for_update(). \
