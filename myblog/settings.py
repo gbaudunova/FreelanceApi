0bd6f1c2933a8739c20201f7b4bf25945da0b61e
Gulbustan <gbaudunova@gmail.com>
2017-10-29 12:47:26 +0600

    Last changes

--- myblog/settings.py
+++ myblog/settings.py
@@ -86,11 +86,25 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'data',
+    'USER': 'root',
+    'PASSWORD': '27101967mirzigul',
+    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    'PORT': '3306',
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        'charset': 'utf8mb4',
     }
+}
 }
 
 # Password validation
