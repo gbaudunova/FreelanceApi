d27dfee6e40b5307b4dcec9abd3fd9c187c787aa
Gulbustan <gbaudunova@gmail.com>
2017-11-01 14:12:52 +0600

    Modified files

--- myblog/settings.py
+++ myblog/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'data',
         'USER': 'root',
         'PASSWORD': '27101967mirzigul',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
 
 }
